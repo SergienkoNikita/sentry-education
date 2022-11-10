9f3956caf7ceb6e6175129fe04d2dd4d736a3e73
SergienkoNikita <SNikita312@yandex.ru>
2022-11-10 16:36:48 +0300

    fix sentry

---
+++ js/about.3636c50e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksenrty_education"]=self["webpackChunksenrty_education"]||[]).push([[443],{2677:function(r,n,e){e.r(n),e.d(n,{default:function(){return d}});var t=e(3396);const o={class:"about"},u=(0,t._)("h1",null,"This is an about page",-1);function s(r,n,e,s,a,c){return(0,t.wg)(),(0,t.iD)("div",o,[u,(0,t._)("button",{onClick:n[0]||(n[0]=(...r)=>c.error&&c.error(...r))},"Ошибка на эбаут")])}var a={methods:{error(){throw new Error("Ошибка на эбаут")}}},c=e(89);const i=(0,c.Z)(a,[["render",s]]);var d=i}}]);
+//# sourceMappingURL=about.3636c50e.js.map
